e7e98da210ee380d3dac538492076b295da8e22e
Shvbham Bisht <158472360+ShvbhamBisht@users.noreply.github.com>
2024-03-05 19:15:37 +0530

    dataTypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,24 @@
+"use script"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.js, not browser
+
+let name = "shubham"
+let age = 19
+let isLoggedIn = false
+let state;
+
+/*
+Primitive DataTypes
+number => 2 to pwer 53
+bigint
+string=> ""
+boolean => true/false
+null => standalone value
+undefined => 
+synbol => unique
+*/
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //null
---
+++ basics/variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "shubham@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 //not allowed
+// accountId = 3554421
+accountEmail = "sb@sb.com"
+accountPassword = "213321"
+accountCity = "dehradun"
+let accountState;
+
+console.log(accountId);
+accountName = "shubham"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope 
+*/
+
+
+console.table([accountId, accountEmail,accountPassword,accountCity, accountName, accountState])
